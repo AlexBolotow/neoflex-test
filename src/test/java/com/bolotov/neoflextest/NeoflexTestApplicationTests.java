0afa6e9711b9c721a12973136d77a97dfc418237
AlexBolotow <bolotow27@gmail.com>
2024-09-16 15:46:11 +0300

    Initial commit

---
+++ src/test/java/com/bolotov/neoflextest/NeoflexTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolotov.neoflextest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
